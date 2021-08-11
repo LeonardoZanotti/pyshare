dd9576d58ae7b79e520e3819c54ae44785c7dfce
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2021-08-11 11:13:26 -0300

    added environment

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Python program to interact with the Microsoft Sharepoint, specifically the Microsoft Lists
 
 ## Installation
+clone .env.example to .env and fill variables
 ```bash
 $ pip3.7 install SharePlum
+$ pip3.7 install python-decouple
 ```
--- pyshare.py
+++ pyshare.py
@@ -2,13 +2,31 @@
 # Leonardo Zanotti
 # https://github.com/LeonardoZanotti/pyshare
 
+from decouple import config
 from shareplum import Office365, Site
 
+# Get environment variables
+spLogin = config('SP_LOGIN')
+spPassword = config('SP_PASSWORD')
+spLink = config('SP_LINK')
+spSite = config('SP_SITE')
+spList = config('SP_LIST')
+
+print(spLogin, spPassword, spLink, spSite, spList)
+
+# Authentication
 authcookie = Office365('https://abc.sharepoint.com',
                        username='username@abc.com', password='password').GetCookies()
+
+# Login in the SharePoint
 site = Site('https://abc.sharepoint.com/sites/MySharePointSite/',
             authcookie=authcookie)
+
+# Enter the site
 sp_list = site.List('list name')
+
+# Get the list of the site
 data = sp_list.GetListItems('All Items', rowlimit=200)
+
 for item in data:
     print(item)
